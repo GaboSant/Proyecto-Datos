cedfc339f24528ffcd90412eb231462877da45e1
GaboSant <113521532+GaboSant@users.noreply.github.com>
2023-11-11 21:25:36 -0500

    Update conexion a la base.py

--- conexion/conexion a la base.py
+++ conexion/conexion a la base.py
@@ -1,5 +1,15 @@
 import psycopg2 as pg
 
+def row_print(rows, n):            # funcion para imprimir n filas
+    if lenght(rows) <= n:          # si el numero de filas es menor al que se desean mostrar, se imprime todas
+        for row in rows:
+            print(row)
+    else:                          # si no, se imprimen el numero filas que se desean
+        i = 0
+        while i != n:
+            print(rows[i])
+            i += 1
+
 try:
     conexion = pg.connect(
         host = "localhost",         # Nombre del host (normalmente es localhost)
@@ -8,52 +18,41 @@ try:
         password = "123456789",     # Contraseña del servidor
         port = "5432")
     cur = conexion.cursor()
+
+    num_filas = 20                  # numero de filas que se desean mostrar
     
     print("tabla ciudad:")
     cur.execute("select * from ciudad")
     rows = cur.fetchall()
-    i = 0
-    while i != 20:
-        print(rows[i])
-        i += 1
+    row_print(rows, num_filas)
         
     print("")
         
     print("tabla recurso:")
     cur.execute("select * from recurso")
     rows = cur.fetchall()
-    i = 0
-    while i != 20:
-        print(rows[i])
-        i += 1
+    row_print(rows, num_filas)
     
     print("")
     
     print("tabla tiene:")
     cur.execute("select * from tiene")
     rows = cur.fetchall()
-    i = 0
-    while i != 20:
-        print(rows[i])
-        i += 1
+    row_print(rows, num_filas)
         
     print("")
     
     print("tabla proyecto:")
     cur.execute("select * from proyecto")
     rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    row_print(rows, num_filas)
         
     print("")
     
     print("tabla explota:")
     cur.execute("select * from explota")
     rows = cur.fetchall()
-    i = 0
-    while i != 20:
-        print(rows[i])
-        i += 1
+    row_print(rows, num_filas)
     
 except Exception as ex:
     print(ex)
